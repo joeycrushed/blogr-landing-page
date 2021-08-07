ba5a3ab50d781c2e61029a271822e04a95238c2e
Joe Smith <joeycrushed@gmail.com>
2021-08-07 10:30:04 +0100

    react navbar

---
+++ blogr-landing-page/src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Products ',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Company',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
