a245edf8e038d8b6ac23d20885819e622380c5c9
Joe Smith <joeycrushed@gmail.com>
2021-08-09 13:47:51 +0100

    Added Introduction component

--- blogr-landing-page/src/App.js
+++ blogr-landing-page/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hero from './components/Hero'
+import Introduction from './components/Introduction'
 
 function App() {
   return (
     <div className="app">
       <Hero />
+      <Introduction />
     </div>
   );
 }
---
+++ blogr-landing-page/src/components/Introduction.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Introduction() {
+    return (
+        <div>
+            <h1>Designed for the future</h1>
+            <div>
+                <div>
+                    <h2>Introducing an extensible editor</h2>
+                    <p>Blogr features an exceedingly intuitive interface</p>
+                    <h2>Robust content management</h2>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Introduction
