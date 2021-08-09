8d8fb0d3684abb0c8ba7bf8655b7e0dfcce6ade4
Joe Smith <joeycrushed@gmail.com>
2021-08-09 20:05:02 +0100

    Add border radius to phone section and image

--- blogr-landing-page/src/components/PhoneSection.js
+++ blogr-landing-page/src/components/PhoneSection.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './PhoneSection.css';
+import phones from '../images/phones.svg'
 
 function PhoneSection() {
     return (
         <div className="phone-bg">
+        <img className="phone-img" src={phones} alt="" />
             <div className="phone-title">
                 <h1>State of the Art Infrastructure</h1>
                 <p>With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
