678329da9b942a90d108bd3a5078bcb8b43a7971
Joe Smith <joeycrushed@gmail.com>
2021-08-09 20:18:11 +0100

    tried to add SVG to hero

--- blogr-landing-page/src/components/Hero.js
+++ blogr-landing-page/src/components/Hero.js
@@ -2,23 +2,26 @@ import React from 'react'
 import Navbar from './Navbar/Navbar'
 import './Hero.css'
 import { Button } from './Button'
-import bg from '../images/bg.svg'
+// import bg from '../images/bg.svg'
 
 
 function Hero() {
     return (
         <div className="hero-bg">
-            <div>
+
+            <div className="nav">
             <Navbar />
             </div>
-            <div className="hero-text">
+
+            <div className="hero">
                 <h1>A modern publishing platform</h1>
                 <p>Grow your audience and build your online trend</p>
                 <div className="hero-btns">
                     <Button>Start For Free</Button>
                     <Button>Learn More</Button>
-                </div>
+                </div>   
             </div>
+
         </div>
    
     )
