123cc139233f8ab66ccac18ed0b68a2cb6516dbc
Joe Smith <joeycrushed@gmail.com>
2021-08-10 14:04:56 +0100

    restart phoneSection

--- blogr-landing-page/src/components/PhoneSection.js
+++ blogr-landing-page/src/components/PhoneSection.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import './PhoneSection.css';
 import phones from '../images/phones.svg'
+import bgcircles from '../images/bg-circles.svg'
 
 function PhoneSection() {
     return (
         <div className="phone-bg">
-        <img className="phone-img" src={phones} alt="" />
-            <div className="phone-title">
-                <h1>State of the Art Infrastructure</h1>
-                <p>With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
-                This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
-                </p>
-            </div>
+            
         </div>
     )
 }
