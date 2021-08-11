58be01235d2d7f82eda849d39b2e04109a9b630a
Joe Smith <joeycrushed@gmail.com>
2021-08-11 12:25:10 +0100

    Add links to footer

--- blogr-landing-page/src/components/Footer.js
+++ blogr-landing-page/src/components/Footer.js
@@ -6,9 +6,33 @@ function Footer() {
         <div className="footer-bg">
             <div className="container">
                 <h1 className="logo">Blogr</h1>
-                <ul>
-                    
-                </ul>
+                <div>
+                    <ul className="footer-list">
+                    <li><h3>Product</h3></li>
+                        <li>Overview</li>
+                        <li>Pricing</li>
+                        <li>Marketplace</li>
+                        <li>Features</li>
+                        <li>Integrations</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className="footer-list">
+                        <li><h3>Company</h3></li>
+                        <li>About</li>
+                        <li>Team</li>
+                        <li>Blog</li>
+                        <li>Careers</li>
+                    </ul>
+                </div>
+                <div >
+                    <ul className="footer-list">
+                    <li><h3>Connect</h3></li>
+                        <li>Contact</li>
+                        <li>Newsletter</li>
+                        <li>LinkedIn</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
